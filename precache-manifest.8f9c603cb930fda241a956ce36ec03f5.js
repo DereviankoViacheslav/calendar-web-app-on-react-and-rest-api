56a49811605eeffd84b2e7bf882fbe2b622e5700
Viacheslav Derevianko <derevianko.viacheslav@gmail.com>
2020-02-03 13:49:03 +0200

    Updates

---
+++ precache-manifest.8f9c603cb930fda241a956ce36ec03f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0310ffd36e320eba0491158c06a38f4a",
+    "url": "/calendar-web-app-on-react-and-rest-api/index.html"
+  },
+  {
+    "revision": "8c96d9509b89b7d70459",
+    "url": "/calendar-web-app-on-react-and-rest-api/static/css/main.a3642a03.chunk.css"
+  },
+  {
+    "revision": "bed969d0ecaafd1a3ea4",
+    "url": "/calendar-web-app-on-react-and-rest-api/static/js/2.771b562e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calendar-web-app-on-react-and-rest-api/static/js/2.771b562e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c96d9509b89b7d70459",
+    "url": "/calendar-web-app-on-react-and-rest-api/static/js/main.900349c0.chunk.js"
+  },
+  {
+    "revision": "eca0ce3121fb9ecf4d1d",
+    "url": "/calendar-web-app-on-react-and-rest-api/static/js/runtime-main.963bc207.js"
+  }
+]);
